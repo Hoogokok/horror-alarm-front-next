c51a537369ae0a330ae85f285d4b1df66c7c3325
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-25 17:57:00 +0900

    refactor: 홈페이지에 이미지 섹션을 추가하고 이미지 스크롤을 개선했다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,22 +14,22 @@ export default function Home() {
         <ul className={styles.list}>
           <li className={styles.menu}>
             <Link href="/upcoming">
-              개봉 예정 영화
+              개봉 예정
             </Link>
           </li>
           <li className={styles.menu}>
             <Link href="/now-playing">
-              현재 상영 중인 영화
+              상영중
             </Link>
           </li>
           <li className={styles.menu}>
             <Link href="/streaming">
-              스트리밍 종료 예정 영화
+              스트리밍 종료 예정
             </Link>
           </li>
           <li className={styles.menu}>
             <Link href="/top-rated">
-              인기 있는 영화
+              인기 영화
             </Link>
           </li>
           <li className={styles.menu}>
@@ -37,96 +37,116 @@ export default function Home() {
           </li>
         </ul>
       </nav>
-      <div className={styles.content}>
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-      </div>
-      <div className={styles.content}>
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-      </div>
-      <div className={styles.content}>
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-        <Image
-          alt="개봉 예정 영화"
-          src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-          width={250}
-          height={300}
-          style={imageStyle}
-        />
-      </div>
+      <section className={styles.imagesection}>
+        <div className={styles.imagesectionTitle}>개봉 예정</div>
+        <div className={styles.content}>
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+        </div>
+        <div className={styles.imagesectionTitle}>상영중</div>
+        <div className={styles.content}>
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+           <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+        </div>
+        <div className={styles.imagesectionTitle}>스트리밍 종료 예정</div>
+        <div className={styles.content}>
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+          <Image
+            alt="개봉 예정 영화"
+            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
+            width={250}
+            height={300}
+            style={imageStyle}
+          />
+        </div>
+      </section>
     </main>
   );
 }
