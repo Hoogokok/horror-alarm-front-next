7ade2c58e909aecc1de83cbc6d5c794f8cb223eb
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-25 12:56:31 +0900

    refactor: 외부 이미지 url을  요청하기 위한 설정을 추가했다.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: process.env.POSTER_HOSTNAME,
+                port: '',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
