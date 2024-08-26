f7bb286d5e62cfb26a8da831afbff94ffe4f56b6
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-26 11:38:43 +0900

    refactor:이미지 지연 로딩을 추가했다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { Suspense } from "react";
+import Loading from "./loading";
 import styles from "./page.module.css";
-import Link from 'next/link'
-import Image from 'next/image'
 
 const imageStyle = {
   padding: '10px',
   borderRadius: '20px',
 }
 
-export default function Home() {
+export default async function Home() {
+  const upcoming = await fetch(process.env.MOVIE_API + '/api/upcoming', { cache: 'force-cache', next: { revalidate: 7200 } }).then(res => res.json());
+  const nowPlaying = await fetch(process.env.MOVIE_API + '/api/releasing', { cache: 'force-cache' }).then(res => res.json());
+  const streamingExpiring = await fetch(process.env.MOVIE_API + '/api/streaming/expired', { cache: 'force-cache' }).then(res => res.json());
+  const streaming = await fetch(process.env.MOVIE_API + '/api/streaming/netflix', { cache: 'force-cache' }).then(res => res.json());
   return (
     <main className={styles.main}>
       <nav className={styles.navigation}>
@@ -38,114 +44,68 @@ export default function Home() {
         </ul>
       </nav>
       <section className={styles.imagesection}>
-        <div className={styles.imagesectionTitle}>개봉 예정</div>
-        <div className={styles.content}>
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-        </div>
-        <div className={styles.imagesectionTitle}>상영중</div>
-        <div className={styles.content}>
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-           <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-        </div>
-        <div className={styles.imagesectionTitle}>스트리밍 종료 예정</div>
-        <div className={styles.content}>
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-          <Image
-            alt="개봉 예정 영화"
-            src={process.env.POSTER_URL + '/gwk23mfvbNzh0zqygZChXlqHbnq.jpg'}
-            width={250}
-            height={300}
-            style={imageStyle}
-          />
-        </div>
+        <Suspense fallback={<Loading />}>
+          <div className={styles.imagesectionTitle}>개봉 예정</div>
+          {
+            upcoming.length ? <div className={styles.content}>
+              {upcoming.map((movie: any) => (
+                <Image
+                  key={movie.id}
+                  alt={movie.title}
+                  src={process.env.POSTER_URL + movie.posterPath}
+                  width={250}
+                  height={300}
+                  style={imageStyle}
+                />
+              ))}
+            </div> : <div className={styles.content}>개봉 예정인 영화가 없습니다.</div>
+          }
+          <div className={styles.imagesectionTitle}>상영중</div>
+          {
+            nowPlaying.length ? <div className={styles.content}>
+              {nowPlaying.map((movie: any) => (
+                <Image
+                  key={movie.id}
+                  alt={movie.title}
+                  src={process.env.POSTER_URL + movie.posterPath}
+                  width={250}
+                  height={300}
+                  style={imageStyle}
+                />
+              ))}
+            </div> : <div className={styles.content}>상영중인 영화가 없습니다.</div>
+          }
+          {
+            streamingExpiring.length ? <div className={styles.imagesectionTitle}>스트리밍 종료 예정</div>
+              : <div className={styles.imagesectionTitle}>넷플릭스 호러</div>
+          }
+          {
+            streamingExpiring.length ? <div className={styles.content}>
+              {streamingExpiring.map((movie: any) => (
+                console.log(movie),
+                <Image
+                  key={movie.id}
+                  alt={movie.title}
+                  src={process.env.POSTER_URL + movie.posterPath}
+                  width={250}
+                  height={300}
+                  style={imageStyle}
+                />
+              ))}
+            </div> : <div className={styles.content}>
+              {streaming.map((movie: any) => (
+                <Image
+                  key={movie.id}
+                  alt={movie.title}
+                  src={process.env.POSTER_URL + movie.poster_path}
+                  width={250}
+                  height={300}
+                  style={imageStyle}
+                />
+              ))}
+            </div>
+          }
+        </Suspense>
       </section>
     </main>
   );
