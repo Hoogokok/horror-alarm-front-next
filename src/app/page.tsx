eba9428bfc69e05c7608527b89b4c2e5f5103aa5
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-08-29 13:51:54 +0900

    feat: 넷플릭스 종료 타입을 추가하고, 값을 꺼내오는 로직을 추가했다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,23 @@ const imageStyle = {
   padding: '10px',
   borderRadius: '20px',
 }
+export interface NetflixResponses {
+  expiredMovies: Array<NetflixResponse>;
+}
+
+interface NetflixResponse {
+  id: string;
+  title: string;
+  posterPath: string;
+  expiredDate: string;
+}
 
 export default async function Home() {
   const upcoming = await fetch(process.env.MOVIE_API + '/api/upcoming', { cache: 'force-cache', next: { revalidate: 7200 } }).then(res => res.json());
   const nowPlaying = await fetch(process.env.MOVIE_API + '/api/releasing', { cache: 'force-cache' }).then(res => res.json());
   const streamingExpiring = await fetch(process.env.MOVIE_API + '/api/streaming/expired', { cache: 'force-cache' }).then(res => res.json());
   const streaming = await fetch(process.env.MOVIE_API + '/api/streaming/netflix', {cache: "force-cache"}).then(res => res.json());
+  const expiredList = streamingExpiring?.expiredMovies
 
   return (
     <main className={styles.main}>
@@ -62,17 +73,17 @@ export default async function Home() {
             )
           }
           {
-            streamingExpiring.length ? <div className={styles.imagesectionTitle}>스트리밍 종료 예정</div>
+            expiredList.length ? <div className={styles.imagesectionTitle}>스트리밍 종료 예정</div>
               : <div className={styles.imagesectionTitle}>넷플릭스 호러</div>
           }
           {
-            streamingExpiring.length ? <div className={styles.content}>
+            expiredList.length ? <div className={styles.content}>
               <div className={styles.content}>
-                {streamingExpiring.map((movie: any) => (
+                {expiredList.map((movie: any) => (
                   <div key={movie.id} className={styles.movieItem}>
                     <Image
                       alt={movie.title}
-                      src={process.env.POSTER_URL + movie.poster_path}
+                      src={process.env.POSTER_URL + movie.posterPath}
                       width={250}
                       height={300}
                       style={imageStyle}
