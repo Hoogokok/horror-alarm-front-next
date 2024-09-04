b6c6a64d19c8b56584c4cf5689fef2172ad53658
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-04 19:11:23 +0900

    refactor: [slug] 디렉토리 삭제
    각 스트리밍 회사 영화를 구분하는 방식을 변경했기에 불필요한 [slug] 파일을 삭제함

--- src/app/streaming/page.tsx
+++ src/app/streaming/page.tsx
@@ -3,16 +3,17 @@ import styles from "../[slug]/page.module.css";
 import { Suspense } from "react";
 import ImageTabs from './components/imageTabs';
 
-export default async function StreamingPage() {
+export default async function StreamingPage({ params }: { params: { slug: string } }) {
     const netflix = await fetch(process.env.MOVIE_API + '/api/streaming/1', { cache: "force-cache" }).then(res => res.json());
     const diseney = await fetch(process.env.MOVIE_API + '/api/streaming/2', { cache: "force-cache" }).then(res => res.json());
     const movies = netflix.map((movie: any) => { return { ...movie, platform: 'netflix' } }
     ).concat(diseney.map((movie: any) => { return { ...movie, platform: 'disney' } })).filter((movie: any) => movie.posterPath);
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <div className={styles.main}>
-                
-            </div>
-        </Suspense>
+        <div>
+            <h1 className={styles.title}>스트리밍</h1>
+            <Suspense fallback={<div>Loading...</div>}>
+                <ImageTabs initialMovies={movies} />
+            </Suspense>
+        </div>
     );
 }
