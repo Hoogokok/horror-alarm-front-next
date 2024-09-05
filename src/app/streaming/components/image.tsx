26e62b130cfa2bcfae092ea1187a5ff2ec1b9862
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-05 19:16:51 +0900

    feat: 이미지 탭 컴포넌트를 업데이트하여 이미지 지연 로딩 추가

--- src/app/streaming/components/image.tsx
+++ src/app/streaming/components/image.tsx
@@ -1,15 +1,15 @@
+import Image from 'next/image';
+
 
 export default async function ImageTabs({ query, page }: { query: string, page: string }) {
-
   const data = await fetch(`${process.env.MOVIE_API}/api/streaming/page?query=${query}&page=${page}`).then(res => res.json());
-  console.log(data);
   return (
     <div>
       {data.map((movie: any) => (
         <div key={movie.id}>
-          <h1>
-            {movie.title}
-          </h1>
+          <Image src={process.env.POSTER_URL + movie.posterPath} alt={movie.title} width={263} height={394}
+          ></Image>
+          <p>{movie.title}</p>
         </div>
       ))}
     </div>
