22553335d7e2aea054f589b032b327a1b23db986
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-05 20:01:40 +0900

    refactor: 스트리밍 페이지 이미지 탭 컴포넌트 스타일 업데이트
    이미지 탭 컴포넌트의 스타일을 업데이트하여  이미지 간의 간격을 조정했다.

--- src/app/streaming/components/image.tsx
+++ src/app/streaming/components/image.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
+import styls from './components.module.css';
 
 
 export default async function ImageTabs({ query, page }: { query: string, page: string }) {
   const data = await fetch(`${process.env.MOVIE_API}/api/streaming/page?query=${query}&page=${page}`).then(res => res.json());
   return (
-    <div>
+    <div className={styls.imagesection}>
       {data.map((movie: any) => (
-        <div key={movie.id}>
+        <div key={movie.id} className={styls.image}>
           <Image src={process.env.POSTER_URL + movie.posterPath} alt={movie.title} width={263} height={394}
           ></Image>
           <p>{movie.title}</p>
