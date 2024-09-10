1f00a8fe2874292447f06b8f1b9f69fbd7287208
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-10 11:25:33 +0900

    feat: 로그인 페이지 컴포넌트 추가
    이 커밋은 이메일과 비밀번호를 위한 양식 입력, 유효성 검사 오류 처리, 제출 버튼이 포함된 새로운 로그인 페이지 컴포넌트를 추가한다. 로그인 양식에 대한 CSS 스타일도 포함되어 있다.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import Link from 'next/link'
+import {login} from './actions'
+import { useActionState } from 'react';
+import styles from './login.module.css'
+
+export default function LoginPage() {
+  const initialState = {
+    error: {
+        email: undefined as string[] | undefined,
+        password: undefined as string[] | undefined,
+    },
+    message: '',
+  }
+  const [state, formAction] = useActionState(login, initialState)
+  
+  return (
+    <div className={styles.container}>
+      <form action={formAction} className={styles.form}>
+        <input type="email" name="email" className={styles.input} />
+        {state.error.email && <p className={styles.error}>{state.error.email[0]}</p>}
+        <input type="password" name="password" className={styles.input} />
+        {state.error.password && <p className={styles.error}>{state.error.password[0]}</p>}
+        <button type="submit" className={styles.button}>로그인</button>
+      </form>
+      <Link href="/signup" className={styles.link}>회원가입</Link>
+    </div>
+  )
+}
