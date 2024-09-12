7f9d8e1380db42fefa20c49d6e6dcc49ec731130
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-13 08:15:37 +0900

    feat: 프로필 컴포넌트에 간단한 목업 프로필 이미지와 css 스타일을 추가

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,28 @@
-'use client';
 import { logout } from "../auth/lib/actions";
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from "./profile.module.css"
+import { getProfile } from "../auth/lib/actions";
 
-export default function Page() {
+export default async function Page() {
+  const profile = await getProfile()
+  const profileImage = null;
+
   return (
-    <div>
+    <div className={styles.profileContainer}>
+      {profileImage ? (
+        <Image src={profileImage} alt="프로필 이미지" width={200} height={200} className={styles.profileImage} />
+      ) : (
+        <div className={styles.profileImagePlaceholder}>
+          ?
+        </div>
+      )}
+      <div className={styles.profileNickname}>{profile.name}</div>
+      <Link href="/edit-profile" className={styles.profileEditLink}>
+        프로필 변경
+      </Link>
       <form action={logout}>
-            <button type="submit">로그아웃</button>
+        <button type="submit" className={styles.logoutButton}>로그아웃</button>
       </form>
     </div>
   );
