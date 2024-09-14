3d4491260fe106de9952dd9226547d53952e1687
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-14 10:49:31 +0900

    feat: auth/lib의 로그인 단위 테스트 추가
    이 커밋은 auth/lib 디렉터리에 로그인 기능에 대한 단위 테스트를 추가합니다. 이 테스트는 성공적인 로그인 시나리오, 이메일 및 비밀번호에 대한 유효성 검사 실패 시나리오, Supabase 오류 처리를 다룹니다. 이 테스트는 로그인 작업이 예상대로 작동하고 다양한 시나리오를 올바르게 처리하는지 확인합니다.

--- src/__tests__/auth/lib/actions.test.ts
+++ src/__tests__/auth/lib/actions.test.ts
@@ -1,5 +1,5 @@
 import { vi, describe, it, expect, beforeEach } from 'vitest'
-import { signup, SignupState } from '@/app/auth/lib/actions'
+import { login, LoginState, signup, SignupState } from '@/app/auth/lib/actions'
 import { createClient } from '@/app/utils/supabase/server'
 import { redirect } from 'next/navigation'
 import { revalidatePath } from 'next/cache'
@@ -9,6 +9,7 @@ vi.mock('@/app/utils/supabase/server', () => ({
     createClient: vi.fn(() => ({
       auth: {
         signUp: vi.fn(),
+        signInWithPassword: vi.fn(),
       },
     })),
   }))
@@ -100,5 +101,79 @@ describe('signup', () => {
     await signup(prevState, formData)
 
     expect(redirect).toHaveBeenCalledWith('/signup')
+  })
+})
+
+describe('login', () => {
+  let prevState: LoginState
+  let formData: FormData
+
+  beforeEach(() => {
+    prevState = {
+      error: undefined,
+      message: undefined,
+    }
+    formData = new FormData()
+    formData.append('email', 'test@example.com')
+    formData.append('password', 'password123!')
+
+    vi.clearAllMocks()
+  })
+
+  it('성공적으로 로그인을 수행해야 합니다', async () => {
+    const mockSignInWithPassword = vi.fn().mockResolvedValue({ error: null })
+    vi.mocked(createClient).mockReturnValue({
+      auth: {
+        signInWithPassword: mockSignInWithPassword,
+      },
+    } as any)
+
+    await login(prevState, formData)
+
+    expect(mockSignInWithPassword).toHaveBeenCalledWith({
+      email: 'test@example.com',
+      password: 'password123!',
+    })
+    expect(revalidatePath).toHaveBeenCalledWith('/', 'layout')
+    expect(redirect).toHaveBeenCalledWith('/')
+  })
+
+  it('유효성 검사 실패 시(이메일 형식이 올바르지 않습니다) 에러를 반환해야 합니다', async () => {
+    formData.set('email', 'invalid-email')
+
+    const result = await login(prevState, formData)
+
+    expect(result).toEqual({
+      error: expect.objectContaining({
+        email: expect.arrayContaining(['이메일 형식이 올바르지 않습니다']),
+      }),
+      message: "로그인 실패"
+    })
+  })
+
+  it('유효성 검사 실패 시(비밀번호 최소 8자 이상이어야 합니다) 에러를 반환해야 합니다', async () => {
+    formData.set('password', 'short')
+
+    const result = await login(prevState, formData)
+
+    expect(result).toEqual({
+      error: expect.objectContaining({
+        password: expect.arrayContaining(['비밀번호는 최소 8자 이상이어야 합니다']),
+      }),
+      message: "로그인 실패"
+    })
+  })
+
+  it('Supabase 에러 발생 시(로그인 실패) 리다이렉트해야 합니다', async () => {
+    const mockSignInWithPassword = vi.fn().mockResolvedValue({ error: new Error('Supabase error') })
+    vi.mocked(createClient).mockReturnValue({
+      auth: {
+        signInWithPassword: mockSignInWithPassword,
+      },
+    } as any)
+
+    await login(prevState, formData)
+
+    expect(redirect).toHaveBeenCalledWith('/login')
   })
 })
