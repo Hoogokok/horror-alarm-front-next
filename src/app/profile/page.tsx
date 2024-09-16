df1dc43014aad13d5a6c67cd279fa4e36900b138
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-16 09:25:19 +0900

    refactor: 프로필 페이지 파일 이름 변경 및 함수 이름 수정

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,8 +2,8 @@ import { getProfile } from "@/app/auth/lib/actions";
 import ProfileEdit from "./edit/page";
 import styles from '@/app/profile/profile.module.css';
 
-export default async function EditProfilePage() {
-    const profile = await getProfile()
+export default async function ProfilePage() {
+  const profile = await getProfile()
   return (
     <div className={styles.profileContainer}>
       <h1 className={styles.title}>프로필 수정</h1>
