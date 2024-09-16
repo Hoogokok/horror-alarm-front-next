008b5b62dc8406ade5abb6f397d8e5c01aac202d
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-16 17:13:38 +0900

    refactor: 영화 상세 페이지에  모바일 반응성 추가

--- src/app/movie/[id]/[category]/components/tabs.tsx
+++ src/app/movie/[id]/[category]/components/tabs.tsx
@@ -69,17 +69,17 @@ export default function Tabs({ movie, user, movieIds, category }: { movie: any, 
       case 'ratings':
         return (
           <div>
-            <p>평점: {movie.voteAverage ? movie.voteAverage : 0}</p>
+            <p>평점: {movie.voteAverage ? movie.voteAverage.toFixed(1) : 0}</p>
             {isLogin && !isRated && (
               <div>
-               <form action={formAction} className={styles.form}>
-                <StarRating rating={rating} setRating={setRating} />
-                <input type="hidden" name="user_id" value={user.id} />
-                <input type="hidden" name="movie_id" value={movie.id} />
-                <input type="hidden" name="category" value={category} />
-                <button>평점 남기기</button>
-               </form>
-               {state.error && <p>{state.message}</p>}
+                <form action={formAction} className={styles.ratingForm}>
+                  <StarRating rating={rating} setRating={setRating} />
+                  <input type="hidden" name="user_id" value={user.id} />
+                  <input type="hidden" name="movie_id" value={movie.id} />
+                  <input type="hidden" name="category" value={category} />
+                  <button type="submit">평점 남기기</button>
+                </form>
+                {state.error && <p>{state.message}</p>}
               </div>
             )}
             {isRated && <p className={styles.rated}>이미 평점을 매겼습니다.</p>}
@@ -114,23 +114,19 @@ export default function Tabs({ movie, user, movieIds, category }: { movie: any, 
 const StarRating = ({ rating, setRating }: { rating: number, setRating: (rating: number) => void }) => {
   return (
     <div className={styles.starRating}>
-    {[1, 2, 3, 4, 5].map((star) => (
-      <label key={star}>
-        <input
-          type="radio"
-          name="rating"
-          value={star}
-          checked={rating === star}
-          onChange={() => setRating(star)}
-          style={{ display: 'none' }}
-        />
-        <span
-          style={{ cursor: 'pointer', color: star <= rating ? 'gold' : 'gray' }}
-        >
-          ★
-        </span>
-      </label>
-    ))}
-  </div>
+      {[1, 2, 3, 4, 5].map((star) => (
+        <label key={star}>
+          <input
+            type="radio"
+            name="rating"
+            value={star}
+            checked={rating === star}
+            onChange={() => setRating(star)}
+            style={{ display: 'none' }}
+          />
+          <span className={star <= rating ? styles.filled : ''}>★</span>
+        </label>
+      ))}
+    </div>
   );
 };
