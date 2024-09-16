c1ee4cd02167e34a0a2884cb2d616e99193c3b23
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-16 12:57:54 +0900

    refactor: 기본 페이지에 도현 글꼴 스타일 적용하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@ import Link from 'next/link';
 import { Suspense } from "react";
 import Loading from "./loading";
 import styles from "./page.module.css";
+import { Do_Hyeon } from "next/font/google";
 
-const imageStyle = {
-  padding: '10px',
-  borderRadius: '20px',
-}
+const doHyeon = Do_Hyeon({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 export interface NetflixResponses {
   expiredMovies: Array<NetflixResponse>;
 }
@@ -26,7 +29,7 @@ export default async function Home() {
   const expiredList = streamingExpiring?.expiredMovies
   
   return (
-    <main className={styles.main}>
+    <main className={styles.main} style={doHyeon.style}>
       <section className={styles.imagesection}>
         <Suspense fallback={<Loading />}>
           <div className={styles.imagesectionTitle}>개봉 예정</div>
