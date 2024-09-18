a099a7a07382d6db7cdcaaddb82dd7f3f8527b2f
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-18 13:23:41 +0900

    refactor: 영화 리뷰 액션 함수를 추가하고 입력 값에 로그인한 사용자가 리뷰한 영화 아이디 값을 추가

--- src/app/movie/[id]/[category]/components/tabs.tsx
+++ src/app/movie/[id]/[category]/components/tabs.tsx
@@ -2,22 +2,29 @@
 
 import { useState } from 'react';
 import { useActionState } from 'react';
-import { rate } from '@/app/movie/lib/actions';
+import { rate, review } from '@/app/movie/lib/actions';
 import styles from "./components.module.css";
 import Link from 'next/link';
 
-export default function Tabs({ movie, user, movieIds, category }: { movie: any, user: any, movieIds: string[], category: string }) {
+export default function Tabs({ movie, user, rate_movieIds, review_movieIds, category }: { movie: any, user: any, rate_movieIds: string[], review_movieIds: string[], category: string }) {
   const initialState = {
     error: {},
     message: ""
   }
-  const [state, formAction] = useActionState(rate, initialState)
+  const initialReviewState = {
+    error: {},
+    message: ""
+  } 
+
+  const [rateState, rateAction] = useActionState(rate, initialState)
+  const [reviewState, reviewAction] = useActionState(review, initialReviewState)
   const [activeTab, setActiveTab] = useState('overview');
   const [currentPage, setCurrentPage] = useState(1);
   const reviewsPerPage = 5;
   const isLogin = user ? true : false;
   const [rating, setRating] = useState(0);
-  const isRated = movieIds.includes(movie.the_movie_db_id)
+  const isRated = rate_movieIds.includes(movie.the_movie_db_id)
+  const isReviewed = review_movieIds.includes(movie.the_movie_db_id)
 
   const reviews = [
     "이 영화는 너무 재미있어요.",
@@ -36,16 +43,6 @@ export default function Tabs({ movie, user, movieIds, category }: { movie: any, 
   const indexOfLastReview = currentPage * reviewsPerPage;
   const indexOfFirstReview = indexOfLastReview - reviewsPerPage;
   const currentReviews = reviews.slice(indexOfFirstReview, indexOfLastReview);
-
-  const [newReview, setNewReview] = useState('');
-
-  const handleReviewSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    // 여기에 리뷰 제출 로직을 구현합니다.
-    console.log('제출된 리뷰:', newReview);
-    // 리뷰를 제출한 후 입력 필드를 초기화합니다.
-    setNewReview('');
-  };
 
   const renderContent = () => {
     switch (activeTab) {
@@ -76,19 +73,26 @@ export default function Tabs({ movie, user, movieIds, category }: { movie: any, 
               </button>
             </div>
 
-            {isLogin ? (
+            {isLogin && !isReviewed ? (
               <div className={styles.reviewForm}>
-                <h3 className={styles.reviewFormTitle}>리뷰 작성하기</h3>
-                <form onSubmit={handleReviewSubmit}>
+                <h3 className={styles.reviewTitle}>리뷰 작성하기</h3>
+                <form action={reviewAction} className={styles.reviewForm} role='form'>
                   <textarea 
-                    value={newReview}
-                    onChange={(e) => setNewReview(e.target.value)}
-                    placeholder="이 영화에 대한 리뷰를 작성해주세요..."
+                    name='review'
+                    placeholder="이 영화에 대한 리뷰를 작성해주세요..." 
+                    className={styles.reviewInput}
                     required
                   />
+                  <input type="hidden" name="movie_id" value={movie.id} />
+                  <input type="hidden" name="user_id" value={user.id} />
+                  <input type="hidden" name="the_movie_db_id" value={movie.the_movie_db_id} />
+                  <input type="hidden" name="category" value={category} />
                   <button type="submit">리뷰 제출</button>
                 </form>
+                {reviewState?.error && <p>{reviewState.message}</p>}
               </div>
+            ) : isReviewed ? (
+              <p className={styles.rated}>이미 리뷰를 작성했습니다.</p>
             ) : (
               <p className={styles.loginPrompt}>
                 <Link href="/login">로그인</Link>하고 리뷰를 작성해보세요!
@@ -102,7 +106,7 @@ export default function Tabs({ movie, user, movieIds, category }: { movie: any, 
             <p>평점: {movie.voteAverage ? movie.voteAverage.toFixed(1) : 0}</p>
             {isLogin && !isRated && (
               <div>
-                <form action={formAction} className={styles.ratingForm}>
+                <form action={rateAction} className={styles.ratingForm}>
                   <StarRating rating={rating} setRating={setRating} />
                   <input type="hidden" name="movie_id" value={movie.id} />
                   <input type="hidden" name="user_id" value={user.id} />
@@ -110,7 +114,7 @@ export default function Tabs({ movie, user, movieIds, category }: { movie: any, 
                   <input type="hidden" name="category" value={category} />
                   <button type="submit">평점 남기기</button>
                 </form>
-                {state.error && <p>{state.message}</p>}
+                {rateState.error && <p>{rateState.message}</p>}
               </div>
             )}
             {isRated && <p className={styles.rated}>이미 평점을 매겼습니다.</p>}
