f14d1ebae485964fd1d65b596332bb880768000c
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-18 13:24:08 +0900

    refactor: 영화 리뷰 액션 함수를 추가

--- src/app/movie/lib/actions.ts
+++ src/app/movie/lib/actions.ts
@@ -55,3 +55,54 @@ export async function rate(prevState: RateState, formData: FormData) {
     revalidatePath(url)
     redirect(url)
 }
+
+const reviewSchema = z.object({
+    movie_id: z.string(),
+    user_id: z.string(),
+    the_movie_db_id: z.string(),
+    category: z.string(),
+    review: z.string().min(1, "리뷰는 1자 이상이어야 합니다").max(1000, "리뷰는 1000자 이하이어야 합니다"),
+})
+
+export type ReviewState = {
+    error?: {
+        movie_id?: string[]
+        user_id?: string[]
+        the_movie_db_id?: string[]
+        category?: string[]
+        review?: string[]
+    }
+    message?: string
+}
+
+export async function review(prevState: ReviewState, formData: FormData) {
+    const supabase = createClient()
+    const validation = reviewSchema.safeParse({
+        movie_id: formData.get('movie_id') as string,
+        user_id: formData.get('user_id') as string,
+        the_movie_db_id: formData.get('the_movie_db_id') as string,
+        category: formData.get('category') as string,
+        review: formData.get('review') as string,
+    })
+
+    if (!validation.success) {
+        const error = validation.error.flatten().fieldErrors
+        console.log(error)
+        return {
+            error: error,
+            message: "리뷰 등록 실패"
+        }
+    }
+    const { movie_id, user_id, the_movie_db_id, category, review } = validation.data
+    const { data, error } = await supabase.from('reviews').insert([{review_movie_id: the_movie_db_id, review_user_id: user_id, review_content: review}])
+    if (error) {
+        console.log(error)
+        return {
+            error: error.message,
+            message: "리뷰 등록 실패"
+        }
+    }
+    const url = `/movie/${movie_id}/${category}`
+    revalidatePath(url)
+    redirect(url)
+}
