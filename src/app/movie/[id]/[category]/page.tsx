e6df01e479878608cf7b2dd6c11f1c7af3b6a8bc
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-19 11:58:30 +0900

    refactor: 영화 포스터 경로 변수명 수정
    영화 포스터 경로 변수명을 `posterPath`에서 `poster_path`로 수정하여 일관성을 유지하도록 변경하였습니다.
    
    개봉일을 변수명을 `releaseDate`에서 `release_date`로 변경하였습니다.
    
    평점 변수명을 `voteAverage`에서 `vote_average`로 변경하셨습니다.

--- src/app/movie/[id]/[category]/page.tsx
+++ src/app/movie/[id]/[category]/page.tsx
@@ -10,7 +10,7 @@ export default async function MovieDetail({ params }: { params: { id: string, ca
   return (
     <div className={styles.main}>
       <div className={styles.imagesection}>
-        <Image src={process.env.POSTER_URL + movie.posterPath} alt={movie.title} width={263} height={394} className={styles.image} />
+        <Image src={process.env.POSTER_URL + movie.poster_path} alt={movie.title} width={263} height={394} className={styles.image} />
         <div className={styles.section}>
           <div className={styles.title}>{movie.title}</div>
           <div className={styles.info}>
