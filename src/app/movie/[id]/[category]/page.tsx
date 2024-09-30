3a75ffc4a6f619bd0c9ea922879c526fd2933ad7
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-01 08:15:10 +0900

    refactor: 영화 상세 정보 API 엔드포인트 구조 개선
    - 'upcoming'과 'released' 카테고리의 엔드포인트를 '/movies/theater/{category}/{id}'로 수정
    - README의 API 명세와 일치하도록 코드 수정
    - switch 문을 사용하여 카테고리별 URL 구성 로직 개선
    
    이 리팩토링으로 백엔드 API 구조와 프론트엔드 코드의 일관성이
    향상되었습니다. 'upcoming'과 'released' 카테고리의 영화에 대해
    각각의 카테고리를 URL에 포함하여 상세 정보를 가져오게 되었으며,
    코드의 가독성과 유지보수성이 개선되었습니다.

--- src/app/movie/[id]/[category]/page.tsx
+++ src/app/movie/[id]/[category]/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import PageTabs from "./components/tabs";
 import { getUser } from "@/app/auth/lib/actions";
 import localFont from 'next/font/local';
+import { MovieDetailResponseDto } from '@/app/types/movie-detail-response-dto';
 
 const doHyeon = localFont({
   src: '../../../fonts/DoHyeon-Regular.ttf',
@@ -10,13 +11,30 @@ const doHyeon = localFont({
 });
 
 export default async function MovieDetail({ params }: { params: { id: string, category: string } }) {
-  const url = `${process.env.MOVIE_API}/api/movie/${params.id}?category=${params.category}`;
-  const movie = await fetch(url, { next: { revalidate: 3600 } }).then(res => res.json());
+  let url: string;
+
+  switch (params.category) {
+    case 'streaming':
+      url = `${process.env.MOVIE_API}/movies/streaming/${params.id}`;
+      break;
+    case 'upcoming':
+    case 'released':
+      url = `${process.env.MOVIE_API}/movies/theater/${params.category}/${params.id}`;
+      break;
+    case 'expiring':
+      url = `${process.env.MOVIE_API}/movies/expiring-horror/${params.id}`;
+      break;
+    default:
+      throw new Error('Invalid category');
+  }
+
+  const movie: MovieDetailResponseDto = await fetch(url, { next: { revalidate: 3600 } }).then(res => res.json());
   const result = await getUser();
+
   return (
     <div className={styles.main}>
       <div className={styles.imagesection}>
-        <Image src={process.env.POSTER_URL + movie.poster_path} alt={movie.title} width={263} height={394} className={styles.image} />
+        <Image src={process.env.POSTER_URL + movie.posterPath} alt={movie.title} width={263} height={394} className={styles.image} />
         <div className={styles.section}>
           <div className={styles.title} style={doHyeon.style}>{movie.title}</div>
           <div className={styles.info}>
