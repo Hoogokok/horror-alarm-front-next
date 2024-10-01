c59e7f8a825acaf829be506554f9f172ea15a5fa
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-01 20:46:15 +0900

    refactor: 영화 타입 위치 변경으로 import 문 변경

--- src/app/streaming/components/image.tsx
+++ src/app/streaming/components/image.tsx
@@ -2,16 +2,12 @@ import Image from 'next/image';
 import styls from './components.module.css';
 import Link from 'next/link';
 import localFont from 'next/font/local';
+import { Movie } from '@/\btypes/movie';
 
 const doHyeon = localFont({
   src: '../../fonts/DoHyeon-Regular.ttf',
   display: 'swap',
 });
-interface Movie {
-  id: number;
-  title: string;
-  posterPath: string;
-}
 
 export default async function ImageTabs({ provider, page }: { provider: string, page: string }) {
   const data: Movie[] = await fetch(`${process.env.MOVIE_API}/movies/streaming?provider=${provider}&page=${page}`, { 
