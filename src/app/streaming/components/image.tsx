44ed50655a639155ba94338be802a84178ba5029
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-01 20:57:03 +0900

    refactor: 이미지 탭 컴포넌트에서 API 유틸리티 함수 사용
    - image.tsx에서 fetchStreamingMovies API 유틸리티 함수 사용
    - 직접적인 fetch 호출을 추상화된 함수로 대체
    - 코드 가독성 및 유지보수성 향상

--- src/app/streaming/components/image.tsx
+++ src/app/streaming/components/image.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import styles from './components.module.css';
 import Link from 'next/link';
 import localFont from 'next/font/local';
+import { fetchStreamingMovies } from '@/utils/api';
 import { Movie } from '@/\btypes/movie';
 
 const doHyeon = localFont({
@@ -10,12 +11,7 @@ const doHyeon = localFont({
 });
 
 export default async function ImageTabs({ provider, page }: { provider: string, page: string }) {
-  const data: Movie[] = await fetch(`${process.env.MOVIE_API}/movies/streaming?provider=${provider}&page=${page}`, { 
-    next: { revalidate: 3600 },
-    headers: {
-      'X-API-Key': process.env.MOVIE_API_KEY as string
-    }
-  }).then(res => res.json());
+  const data = await fetchStreamingMovies(provider, page);
 
   return (
     <div className={styles.movieList}>
@@ -23,14 +19,14 @@ export default async function ImageTabs({ provider, page }: { provider: string, 
         <div key={movie.id} className={styles.movieItem}>
           <div className={styles.moviePosterContainer}>
             <Image 
-                src={process.env.POSTER_URL + movie.posterPath} 
+              src={process.env.POSTER_URL + movie.posterPath} 
               alt={movie.title} 
               className={styles.moviePoster} 
               width={100}
               height={100}
             />
           </div>
-          <Link href={`/movie/${movie.id}/${"streaming"}`} className={`${styles.movieTitle} ${doHyeon.className}`}>
+          <Link href={`/movie/${movie.id}/streaming`} className={`${styles.movieTitle} ${doHyeon.className}`}>
             {movie.title}
           </Link>
         </div>
