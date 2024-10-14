39d4a4221707271a9f17ca612b22f024f9826c2f
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-14 19:19:16 +0900

    feat: YouTube API의 캐싱 기능을 개선하여 최신 동영상을 더욱 효율적으로 가져오기
    - NodeCache의 TTL을 1시간에서 24시간으로 변경하여 캐시 유지 시간을 늘림
    - 캐시된 데이터가 있는 경우에도 API를 호출하여 업데이트된 동영상을 가져오도록 수정
    - 오류 발생 시 이전에 캐시된 데이터를 반환하여 사용자에게 최신 동영상을 보여줌

--- src/utils/youtubeApi.ts
+++ src/utils/youtubeApi.ts
@@ -1,20 +1,20 @@
 import axios from 'axios';
 import NodeCache from 'node-cache';
 
-const API_KEY = process.env.YOUTUBE_API_KEY;
+const API_KEY = process.env.YOUTUBE_API_KEY2;
 const BASE_URL = 'https://www.googleapis.com/youtube/v3';
 
 // 캐시 인스턴스 생성 (기본 TTL: 1시간)
-const cache = new NodeCache({ stdTTL: 3600 });
+const cache = new NodeCache({ stdTTL: 86400 });
 
 export async function getLatestVideo(channelId: string) {
-  // 캐시에서 데이터 확인
   const cachedData = cache.get<any[]>(channelId);
+  //캐시 데이터가 있으면 캐시 데이터 반환
   if (cachedData) {
     console.log(`캐시된 데이터 반환: ${channelId}`);
     return cachedData;
   }
-
+  
   try {
     const response = await axios.get(`${BASE_URL}/search`, {
       params: {
@@ -28,12 +28,11 @@ export async function getLatestVideo(channelId: string) {
     });
     
     if (response.data.items && response.data.items.length > 0) {
-      // 결과를 캐시에 저장
       cache.set(channelId, response.data.items);
       return response.data.items;
     } else {
       console.log(`No videos found for channel ID: ${channelId}`);
-      return null;
+      return cachedData || null;
     }
   } catch (error) {
     if (axios.isAxiosError(error) && error.response) {
@@ -41,6 +40,6 @@ export async function getLatestVideo(channelId: string) {
     } else {
       console.error('YouTube API 요청 중 오류 발생:', error);
     }
-    return null;
+    return cachedData || null;
   }
 }
