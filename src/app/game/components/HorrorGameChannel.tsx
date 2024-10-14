490c31a584a1ed0b5a9bd1a93a10e281f57f6df1
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-14 15:11:42 +0900

    feat: 게임 페이지에 채널 목록을 가로로 스크롤 하는 형태로 수정

--- src/app/game/components/HorrorGameChannel.tsx
+++ src/app/game/components/HorrorGameChannel.tsx
@@ -1,58 +1,63 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { getLatestVideo } from '@/utils/youtubeApi';
+    import React from 'react';
+    import Image from 'next/image';
+    import Link from 'next/link';
+    import { getLatestVideo } from '@/utils/youtubeApi';
+    import styles from '../page.module.css';
 
-interface HorrorGameChannelProps {
-    channelId: string;
-    name: string;
-    channelName: string;
-}
+    interface HorrorGameChannelProps {
+        channelId: string;
+        name: string;
+        channelName: string;
+    }
 
-interface VideoItem {
-    id: { videoId: string };
-    snippet: {
-        title: string;
-        description: string;
-        thumbnails: {
-            medium: {
-                url: string;
+    interface VideoItem {
+        id: { videoId: string };
+        snippet: {
+            title: string;
+            description: string;
+            thumbnails: {
+                medium: {
+                    url: string;
+                };
             };
         };
+    }
+
+    const HorrorGameChannel: React.FC<HorrorGameChannelProps> = async ({ channelId, name, channelName }) => {
+        const channelUrl = `https://www.youtube.com/@${name}`;
+        const latestVideos = await getLatestVideo(channelId);
+        return (
+            <div className={styles.channelItem}>
+                <h3 className={styles.channelName}>{channelName}</h3>
+                <Link href={channelUrl} target="_blank" rel="noopener noreferrer" className={styles.channelLink}>
+                    채널 방문하기
+                </Link>
+                {latestVideos && (
+                <div className={styles.latestVideos}>
+                    {latestVideos.map((video: VideoItem) => (
+                        <div key={video.id.videoId} className={styles.videoItem}>
+                            <div className={styles.thumbnailContainer}>
+                                <Image 
+                                    src={video.snippet.thumbnails.medium.url}
+                                    alt={video.snippet.title}
+                                    fill={true}
+
+                                    className={styles.thumbnail}
+                                />
+                            </div>
+                            <div className={styles.videoInfo}>
+                                <h4 className={styles.videoTitle}>{video.snippet.title}</h4>
+                                <p className={styles.videoDescription}>{video.snippet.description.slice(0, 100)}...</p>
+                                <Link href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank" rel="noopener noreferrer" className={styles.videoLink}>
+                                    영상 보기
+                                </Link>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                )}
+            </div>
+        );
     };
-}
 
-const HorrorGameChannel: React.FC<HorrorGameChannelProps> = async ({ channelId, name, channelName }) => {
-    const channelUrl = `https://www.youtube.com/@${name}`;
-    const latestVideos = await getLatestVideo(channelId);
-
-    return (
-        <div className="channel-item">
-            <h3>{channelName}</h3>
-            <Link href={channelUrl} target="_blank" rel="noopener noreferrer">
-                채널 방문하기
-            </Link>
-            {latestVideos && (
-               <div className="latest-videos">
-                {latestVideos.map((video: VideoItem) => (
-                    <div key={video.id.videoId} className="video-item">
-                        <Image 
-                            src={video.snippet.thumbnails.medium.url}
-                            alt={video.snippet.title}
-                            width={320}
-                            height={180}
-                        />
-                        <h4>{video.snippet.title}</h4>
-                        <p>{video.snippet.description.slice(0, 100)}...</p>
-                        <Link href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank" rel="noopener noreferrer">
-                            영상 보기
-                        </Link>
-                    </div>
-                ))}
-               </div>
-            )}
-        </div>
-    );
-};
-
-export default HorrorGameChannel;
+    export default HorrorGameChannel;
