768153b4dcdb4e5d9881acfd9dea31acd1fe99e8
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-14 21:34:45 +0900

    새로운 공포 게임 채널을 포함하도록 horrorGameChannels.ts 업데이트
    - horrorGameChannels.ts 파일에 새로운 공포 게임 채널 추가
    - 새로 추가된 채널은 "그믐"으로, ID는 "UCOgKBAVZQSOzgKVCZ0dcvug"이며 채널명은 "그믐의 공포게임"입니다

--- src/data/horrorGameChannels.ts
+++ src/data/horrorGameChannels.ts
@@ -1,6 +1,7 @@
 export const horrorGameChannels = [
     { id: 'UCw-JzmPsjRcbzJLncr4pIIA', name: 'sutak_istp', channel_name: '수탉' },
     { id: 'UCxhRaP7gMELgT8Xkzf065Tw', name: 'GS_KSM', channel_name: '강여름 - 게임 스토리' },
-    {id: 'UCA1G6S65RhOh-0IDqAWhIJA', name: '막셋', channel_name: '막셋'}
+    {id: 'UCA1G6S65RhOh-0IDqAWhIJA', name: '막셋', channel_name: '막셋'},
+    {id: 'UCOgKBAVZQSOzgKVCZ0dcvug', name: '그믐', channel_name: '그믐의 공포게임'}
 ];
 
