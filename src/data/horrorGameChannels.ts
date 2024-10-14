b41669bc874bd2536a2b7ffaac18f3747e77bc9a
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-10-14 19:18:57 +0900

    feat: 게임 채널 api 에 필요한 데이터 추가

--- src/data/horrorGameChannels.ts
+++ src/data/horrorGameChannels.ts
@@ -1,4 +1,6 @@
 export const horrorGameChannels = [
-    { id: '@sutak_istp', name: '수탉' }
+    { id: 'UCw-JzmPsjRcbzJLncr4pIIA', name: 'sutak_istp', channel_name: '수탉' },
+    { id: 'UCxhRaP7gMELgT8Xkzf065Tw', name: 'GS_KSM', channel_name: '강여름 - 게임 스토리' },
+    {id: 'UCA1G6S65RhOh-0IDqAWhIJA', name: '막셋', channel_name: '막셋'}
 ];
 
